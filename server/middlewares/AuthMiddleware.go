388c0f94315c10ac429abfd78ddd215b080bc91f
sahsa <sahsaleon1203@gmail.com>
2023-09-05 17:43:13 +0300

    Implementing authMiddleware(with bags)

---
+++ server/middlewares/AuthMiddleware.go
@@ -0,0 +1,36 @@
+package middlewares
+
+import (
+	"context"
+	"github.com/bobul/ostud-educational-platform/service"
+	"net/http"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		operationName := service.GetOperationNameFromRequest(r)
+		if operationName == "UserLogin" || operationName == "UserRegister" {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		const BearerSchema = "Bearer "
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := authHeader[len(BearerSchema):]
+
+		username, err := service.JwtParseToken(tokenString)
+		if err != nil {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		contextWithToken := context.WithValue(r.Context(), "username", username)
+		r = r.WithContext(contextWithToken)
+		next.ServeHTTP(w, r)
+	})
+}
--- server/service/auth.go
+++ server/service/auth.go
@@ -1,8 +1,19 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/graphql-go/graphql/language/ast"
+	"github.com/graphql-go/graphql/language/parser"
 	"golang.org/x/crypto/bcrypt"
+	"io/ioutil"
+	"net/http"
 )
+
+type GraphQLRequest struct {
+	Query     string      `json:"query"`
+	Variables interface{} `json:"variables"`
+}
 
 func HashPassword(s string) string {
 	hashed, _ := bcrypt.GenerateFromPassword([]byte(s), bcrypt.DefaultCost)
@@ -11,4 +22,39 @@ func HashPassword(s string) string {
 
 func ComparePassword(hashed string, normal string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashed), []byte(normal))
+}
+
+func GetOperationNameFromRequest(r *http.Request) string {
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		return fmt.Sprint(err)
+	}
+
+	var request GraphQLRequest
+
+	err = json.Unmarshal(body, &request)
+	if err != nil {
+		return fmt.Sprint(err)
+	}
+	r.Body.Close()
+	return parseOperationName(request.Query)
+}
+
+func parseOperationName(query string) string {
+	// Parse the GraphQL query
+	doc, err := parser.Parse(parser.ParseParams{Source: query})
+	if err != nil {
+		return ""
+	}
+
+	// Iterate through the definitions to find the operation name
+	for _, def := range doc.Definitions {
+		operationDef := def.(*ast.OperationDefinition)
+		if operationDef.Name != nil {
+			return operationDef.Name.Value
+		}
+	}
+
+	return ""
 }
